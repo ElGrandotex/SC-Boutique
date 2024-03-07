8fc37fabd2b3f6fd635240de96762c0b1b7150ea
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-03-07 12:31:33 -0500

    Modulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './SC-Boutique/pages/contact/contact.component';
 import { PrincipalComponent } from './SC-Boutique/pages/principal/principal.component';
-import { AccesoriosComponent } from './SC-Boutique/pages/accesorios/accesorios.component';
 import { BlusasComponent } from './SC-Boutique/pages/blusas/blusas.component';
 import { PantalonesComponent } from './SC-Boutique/pages/pantalones/pantalones.component';
 import { VestidosComponent } from './SC-Boutique/pages/vestidos/vestidos.component';
 import { CartComponent } from './SC-Boutique/pages/cart/cart.component';
+import { ZapatosComponent } from './SC-Boutique/pages/zapatos/zapatos.component';
 
 const routes: Routes = [
   {
@@ -18,16 +18,16 @@ const routes: Routes = [
     component: ContactComponent
   },
   {
-    path: 'accesorios',
-    component: AccesoriosComponent
-  },
-  {
     path: 'blusas',
     component: BlusasComponent
   },
   {
     path: 'pantalones',
     component: PantalonesComponent
+  },
+  {
+    path: 'zapatos',
+    component: ZapatosComponent
   },
   {
     path: 'vestidos',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { SCBoutiqueModule } from './SC-Boutique/sc-boutique.module';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -13,8 +17,12 @@ import { SCBoutiqueModule } from './SC-Boutique/sc-boutique.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgSelectModule,
     SharedModule,
-    SCBoutiqueModule
+    CommonModule,
+    HttpClientModule,
+    SCBoutiqueModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
