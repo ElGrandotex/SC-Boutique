9e0a02c3fbfae97f3ddaee22238d180f9fc206ad
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-03-07 12:33:56 -0500

    Afinamientos

--- src/app/SC-Boutique/sc-boutique.module.ts
+++ src/app/SC-Boutique/sc-boutique.module.ts
@@ -4,12 +4,15 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { RouterModule } from '@angular/router';
 import { PrincipalComponent } from './pages/principal/principal.component';
-import { AccesoriosComponent } from './pages/accesorios/accesorios.component';
 import { BlusasComponent } from './pages/blusas/blusas.component';
 import { PantalonesComponent } from './pages/pantalones/pantalones.component';
 import { VestidosComponent } from './pages/vestidos/vestidos.component';
 import { CartComponent } from './pages/cart/cart.component';
-
+import { ZapatosComponent } from './pages/zapatos/zapatos.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { MatSortModule } from '@angular/material/sort';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -17,15 +20,19 @@ import { CartComponent } from './pages/cart/cart.component';
     ContactComponent,
     HomepageComponent,
     PrincipalComponent,
-    AccesoriosComponent,
     BlusasComponent,
     PantalonesComponent,
     VestidosComponent,
-    CartComponent
+    CartComponent,
+    ZapatosComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
+    NgSelectModule,
+    FormsModule,
+    MatSortModule,
+    NoopAnimationsModule
   ],
   exports: [
     HomepageComponent
