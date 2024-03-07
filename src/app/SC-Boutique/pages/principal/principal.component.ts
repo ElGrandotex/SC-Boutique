ca23c54311a265f75fc93900d0c8ae4e9fe78f5f
ElGrandotex <jose.verdezoto.moran@gmail.com>
2024-03-07 12:33:42 -0500

    HomePage

--- src/app/SC-Boutique/pages/principal/principal.component.ts
+++ src/app/SC-Boutique/pages/principal/principal.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { CarouselItem } from 'src/app/interfaz/carousel-item.interface';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
+  styleUrls: ['./principal.component.css'],
+  animations: [
+    trigger('slide', [
+      transition('hidden => visible', [
+        style({ transform: 'translateX(100%)' }),
+        animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' })),
+      ]),
+      transition('visible => hidden', [
+        animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ],
 })
-export class PrincipalComponent {
+export class PrincipalComponent{
+
 
 }
